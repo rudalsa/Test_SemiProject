07a0b39193d844df29d077ac819d3fd8c67ff918
chyukl <chaehyuk97@gmail.com>
2023-10-24 15:07:24 +0900

    내용물에 변경사항 발생

--- Test_SemiProject/src/main/java/teammem/limch/Teammem_limch.java
+++ Test_SemiProject/src/main/java/teammem/limch/Teammem_limch.java
@@ -5,6 +5,7 @@ public class Teammem_limch {
 	public static void main(String[] args) {
 		// 임채혁 전용 브랜치에 클래스 생성함
 		System.out.println("임채혁 전용 브랜치에 클래스 생성함");
+		System.out.println("내용물에 변경사항 발생");
 	}
 
 }
